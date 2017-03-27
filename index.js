f8b1a06228b8bf3f91626262c9ee0ebe9e38869a
Martin Abelson Sahlen <sahlen@Martins-MacBook-Pro.local>
2017-03-28 00:09:28 +0200

    Make logging work

--- index.js
+++ index.js
@@ -1,16 +1,26 @@
-const spawnSync = require('child_process').spawnSync
+const spawn = require('child_process').spawn
 
+//Handle Background events according to spec
 function backgroundHandler(data) {
-
-  result = spawnSync('./function', [], {
-    input: JSON.stringify(data),
-    stdio: 'pipe',
+  return new Promise((resolve, reject) => {
+    const p = spawn('./function', [], {});
+    p.stdin.setEncoding('utf-8');
+    p.stdin.write(JSON.stringify(data));
+    p.stdin.end();
+    p.stderr.on('data', (err) => {
+      console.error(err.toString());
+    })
+    p.stdout.on('data', (out) => {
+      console.log(out.toString());
+    })
+    p.on('close', (code) => {
+      if (code !== 0) {
+        reject();
+      } else {
+        resolve();
+      }
+    });
   });
-
-  if (result.status !== 0) {
-     throw new Error(result.stderr.toString())
-  }
-  return result.stdout;
 }
 
 exports.helloPubSub = function(event) {
