3144677007db2fd3a906c25c596530d3ab49a99c
Martin Abelson Sahlen <martin8900@gmail.com>
2017-06-01 00:12:47 -0400

    Smoother deploy (#18)
    * F
    
    * Fix emulator deploy
    
    * Add examples
    
    * Add http example
    
    * Fix

--- examples/http/http.go
+++ examples/http/http.go
@@ -1,16 +1,48 @@
 package main
 
 import (
-	"io"
+	"encoding/json"
 	"net/http"
+	"os"
+
+	"google.golang.org/api/iterator"
+
+	"cloud.google.com/go/bigquery"
 
 	"github.com/MartinSahlen/go-cloud-fn/shim"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
+	client, err := bigquery.NewClient(r.Context(), os.Getenv("GCLOUD_PROJECT"))
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	datasetsIterator := client.Datasets(r.Context())
+	datasets := []*bigquery.Dataset{}
+	for {
+		dataset, err := datasetsIterator.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		datasets = append(datasets, dataset)
+	}
+
+	byt, err := json.Marshal(datasets)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	w.Header().Set("Content-Type", "application/json")
-	io.WriteString(w, `{"alive": true}`)
+	w.WriteHeader(http.StatusOK)
+	w.Write(byt)
 }
 
 func main() {
